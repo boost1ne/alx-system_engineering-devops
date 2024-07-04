a5f1287ba8de7bc115e51d701e86b984c422b65c
boost1ne <boustane.chaimaa@gmail.com>
2024-07-03 17:45:11 -0700

    done

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,6 @@
-#!/usr/bin/pup
-# Install flask package v2.1.0 from pip3.
+#install flask package v2.1.0 from pip3.
 
 package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+ensure   => '2.1.0',
+provider => 'pip3',
 }
-
