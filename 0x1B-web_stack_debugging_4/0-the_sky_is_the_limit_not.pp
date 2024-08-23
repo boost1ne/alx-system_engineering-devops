3504cc69c9837708408c71367fa785b64e6db8a9
boost1e <boustane.chaimaa@gmail.com>
2024-08-23 15:25:54 -0700

    fixes adjust the web stack so that it can handle 1000 request with 100 at a time

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -3,6 +3,8 @@ exec { 'max_nginx_traffic':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
 } =>
+
+# Restart Nginx after config changes
 exec { 'restart_nginx':
   command => 'nginx restart',
   path    => '/etc/init.d/'
