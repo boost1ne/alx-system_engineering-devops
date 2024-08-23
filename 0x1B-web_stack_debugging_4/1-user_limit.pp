729d46883aacf2d20aeb566fdf12a61275d21c0a
boost1e <boustane.chaimaa@gmail.com>
2024-08-23 15:41:20 -0700

    user limit

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,11 @@
+# Enable the user holberton to login and open files without error.
+exec { 'holberton_hard_limit':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Increase soft file limit for Holberton user.
+exec { 'holberton_soft_limit':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
